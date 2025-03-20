d1d40e0c45b66b5cec370106b69b8678b3e888ba
mariaclararcs <mariaclarar981@gmail.com>
2025-03-20 09:59:02 -0300

    initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Análise BSC</title>
+</head>
+<body>
+    <h1>Olá, mundo!</h1>
+    <?php
+        echo "index.php";
+    ?>
+</body>
+</html>
